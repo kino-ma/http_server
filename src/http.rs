59d1b7aef9821315d77c6a71190d0f0f808c84ae
kino-ma <kino.ma.ms@gmail.com>
2020-07-14 21:52:43 +0900

    add `Header::new()` test

--- src/http.rs
+++ src/http.rs
@@ -73,6 +73,24 @@ fn to_result<T>(value: Option<T>, msg: &str) -> Result<T> {
 pub mod tests {
     use super::*;
 
+    /*
+    pub fn should_parse_request() {
+        let content = "GET /path/to/resource HTTP/1.0\r\nAccept: *_/_*\r\nAccept-Language: ja\r\nAccept-Encoding: gzip, deflate\r\nUser-Agent: Mozilla/4.0 (Compatible; MSIE 6.0; Windows NT 5.1;)\r\nConnection: Close\r\n";
+        let request = Request::new(content);
+
+        let info = Info {
+            method: "GET",
+            path: "/path/to/resource",
+            protocol_minor_version: 0,
+        };
+
+        let headers = Vec::new()
+        headers: Vec<Header<'a>>,
+        body: &'a str,
+        length: u64,
+    }
+    */
+
     #[test]
     pub fn should_parse_info() {
         let line = "GET /path/to/resource HTTP/1.0";
@@ -84,5 +102,16 @@ pub mod tests {
         };
 
         assert_eq!(info, expect);
+    }
+
+    pub fn should_parse_headers() {
+        let content = "Accept: */*\r\nConnection: Close\r\nUser-Agent: Mozilla/4.0 (Compatible; MSIE 6.0; Windows NT 5.1;)\r\n";
+        let headers = content.lines().map(|line| Header::new(line).expect("failed to parse"));
+
+        let mut expect = Vec::new();
+        expect.push(Header { name: "Accept", value: "*/*" });
+        expect.push(Header { name: "Connection", value: "Close" });
+        expect.push(Header { name: "Accept", value: "*/*" });
+        expect.push(Header { name: "User-Agent", value: "Mozilla/4.0 (Compatible; MSIE 6.0; Windows NT 5.1;" });;
     }
 }
