c7e3435cadcde18a5ce5cceb4ab58aac49b90bc7
kino-ma <kino.ma.ms@gmail.com>
2020-07-14 21:23:00 +0900

    add parsing functions/structs

--- src/http.rs
+++ src/http.rs
@@ -1,12 +1,16 @@
-use std::io::Result;
+use std::io::{self, Result, Error, ErrorKind};
 
 pub struct Request<'a> {
-    protocol_minor_version: i32,
-    method: &'a str,
-    path: &'a str,
+    info: Info<'a>,
     headers: Vec<Header<'a>>,
     body: &'a str,
     length: u64,
+}
+
+pub struct Info<'a> {
+    method: &'a str,
+    path: &'a str,
+    protocol_minor_version: i32,
 }
 
 pub struct Header<'a> {
@@ -16,13 +20,48 @@ pub struct Header<'a> {
 
 impl Request<'_> {
     pub fn new<'a>(src: &'a str) -> Result<Request<'a>> {
-        Ok(Request {
-            protocol_minor_version: 0,
-            method: src,
-            path: src,
+        let mut lines = src.lines();
+
+        let first = to_result(lines.next(), "not enough info")?;
+
+        let info = Info::new(first)?;
+
+        Ok( Request {
+            info,
             headers: Vec::new(),
             body: src,
             length: 1,
         })
     }
+}
+
+impl Info<'_> {
+    pub fn new<'a>(line: &'a str) -> Result<Info<'a>> {
+        let mut words = line.split_whitespace();
+
+        let method = to_result(words.next(), "failed to parse")?;
+        let path = to_result(words.next(), "failed to parse")?;
+        let protocol_minor_version = Self::parse_version(to_result(words.next(), "failed to parse")?)?;
+
+        Ok( Info {
+            method,
+            path,
+            protocol_minor_version,
+        })
+    }
+
+    fn parse_version(src: &str) -> Result<i32> {
+        let pv = src
+            .chars()
+            .last();
+        let pv = to_result(pv, "failed to parse")?
+            .to_digit(10);
+        let pv = to_result(pv, "failed to parse");
+
+        pv.and_then(|x| Ok(x as i32))
+    }
+}
+
+fn to_result<T>(value: Option<T>, msg: &str) -> Result<T> {
+    value.ok_or(Error::new(ErrorKind::Other, msg))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub fn log_exit(text: &str) {
 pub fn service<I: Read, O: Write>(mut input: I, mut output: O, docdir: &str) -> std::io::Result<()> {
     let mut buf = String::new();
 
-    let mut req: http::Request;
+    let mut req = http::Request::new(&buf)?;
 
     input.read_to_string(&mut buf)?;
     output.write_all(&buf.into_bytes())
