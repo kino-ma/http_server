3297fec941d7c438c00b43b3c72ac7710789456c
kino-ma <kino.ma.ms@gmail.com>
2020-07-14 19:55:05 +0900

    add `Request::new()`

--- src/http.rs
+++ src/http.rs
@@ -1,3 +1,5 @@
+use std::io::Result;
+
 pub struct Request<'a> {
     protocol_minor_version: i32,
     method: &'a str,
@@ -10,4 +12,17 @@ pub struct Request<'a> {
 pub struct Header<'a> {
     name: &'a str,
     value: &'a str,
+}
+
+impl Request<'_> {
+    pub fn new<'a>(src: &'a str) -> Result<Request<'a>> {
+        Ok(Request {
+            protocol_minor_version: 0,
+            method: src,
+            path: src,
+            headers: Vec::new(),
+            body: src,
+            length: 1,
+        })
+    }
 }
