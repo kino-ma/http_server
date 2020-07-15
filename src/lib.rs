5f517b6c0b85fc88d581ea9efd7e2ed382191dc4
kino-ma <kino.ma.ms@gmail.com>
2020-07-15 22:55:22 +0900

    change `main.rs` & `lib.rs` to use TcpStream

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod http;
 
 use std::io::prelude::*;
+use std::net::TcpStream;
 
 pub fn log_exit(text: &str) {
     eprintln!("error: {}", text);
@@ -8,12 +9,14 @@ pub fn log_exit(text: &str) {
     std::process::exit(1);
 }
 
-pub fn service<I: Read, O: Write>(mut input: I, mut output: O, docdir: &str) -> std::io::Result<()> {
+pub fn service(mut stream: TcpStream, docdir: &str) -> std::io::Result<()> {
     let mut buf = String::new();
 
-    input.read_to_string(&mut buf)?;
+    stream.read_to_string(&mut buf)?;
 
-    let mut req = http::Request::new(&buf)?;
+    let mut request = http::Request::new(&buf)?;
 
-    output.write_all(&buf.into_bytes())
+    let mut response = http:::Response::new(request)?;
+
+    response.send(stream);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate http_server;
 
 use std::env;
+use std::net::TcpListener;
 
 use http_server::*;
 
@@ -13,7 +14,19 @@ fn main() {
         log_exit("not enough args");
     }
 
-    if let Err(error) = service(std::io::stdin(), std::io::stdout(), &args[1]) {
-        log_exit(&error.to_string());
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("failed to bind port");
+
+    for stream in listener.incoming() {
+        let mut stream = match stream {
+            Ok(stream) => stream,
+            Err(error) => {
+                log_exit(&error.to_string());
+                continue;
+            }
+        };
+
+        if let Err(error) = service(stream, &args[1]) {
+            log_exit(&error.to_string());
+        }
     }
 }
